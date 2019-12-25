c351b3aec69e861040172803196134213efcbcb5
saiemkhan929 <34841315+saiemkhan929@users.noreply.github.com>
2019-12-25 20:04:50 +0100

    Update empty_space.js

--- empty_space.js
+++ empty_space.js
@@ -5,43 +5,19 @@ jQuery(document).ready(function($){
 
       $('.empty_space').each(function(index, el) {
 
-        var dekstop = $(this).attr('data-dekstop');
-        var tab = (null != $(this).attr('data-tab')) ? $(this).attr('data-tab') : dekstop;
-        var mobile = (null != $(this).attr('data-mobile')) ? $(this).attr('data-mobile') : dekstop;
-
-        console.log(dekstop+' '+tab+' '+mobile);
+        var desktop = $(this).attr('data-desktop');
+        var tab = (null != $(this).attr('data-tab')) ? $(this).attr('data-tab') : desktop;
+        var mobile = (null != $(this).attr('data-mobile')) ? $(this).attr('data-mobile') : desktop;
 
         var screen = $(window).width();
 
         if(screen > 1000){
-          $(this).css('height', dekstop);
+          $(this).css('height', desktop);
         }else if(screen > 650 && screen < 1000){
           $(this).css('height', tab);
         }else if(screen < 650 ){
           $(this).css('height', mobile);
         // map setting here
-
-                $('.navbar .dropdown i').click(function(event) {
-                    /* Act on the event */
-                    event.preventDefault();
-
-
-
-                  if($(this).parents('.dropdown').children('.dropdown-menu').css('display') == 'block'){
-
-                  $(this).parents('.dropdown').children('.dropdown-menu').css('display','none');
-                  alert('');
-
-                  }else{
-
-                    $(this).parents('.dropdown').children('.dropdown-menu').css('display','block');
-
-
-                  }
-
-
-                  });
-
         }
 
 
